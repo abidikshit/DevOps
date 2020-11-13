01d163a3abd3ea2119b3dd652c778fd36f247bc2
Abhilash Dikshit <abhilashdikshit23@gmail.com>
2020-11-13 10:28:27 +0000

    Update P4UserDel.py

--- Perforce/P4User_Del/P4UserDel.py
+++ Perforce/P4User_Del/P4UserDel.py
@@ -1,9 +1,6 @@
 #!/p4/common/python/bin/python3
 """
 AUTHOR: Abhilash Dikshit
-PUBLISHER: Abhilash Dikshit
-RELEASE DATE: 14 Sept 2020
-MODIFIED DATE: 30 Oct 2020
 DESCRIPTION: Perforce user deletion of inactive AD users
 """
 
@@ -41,23 +38,21 @@ def del_operation():
                     service_account()
 
             def service_account():
-                check_service_account = "p4 groups -u "+user+" " \
-                                                             "|egrep 'service_users|serviceaccounts|" \
-                                                             "serviceperforce|serviceperforceread'"
+                check_service_account = "p4 groups -u "+user+"| egrep 'service_users|serviceaccounts|serviceperforce|serviceperforceread'"
                 service_account_status = os.system(check_service_account)
                 if service_account_status == 0:
-                    print("'"+user+"' belongs to service account, excluding...")
+                    print("\n'"+user+"' belongs to service account, excluding...")
                 else:
-                    print("'"+user+"' does not belongs to service account, checking for normal/super user...")
+                    print("\n'"+user+"' does not belongs to service account, checking for normal/super user...")
                     superuser()
 
             def superuser():
-                check_superuser = "p4 protect -o | tail -12 | grep -w "+user
+                check_superuser = "p4 protects -u " + user + " | grep 'super user'"
                 check_superuser_status = os.system(check_superuser)
                 if check_superuser_status == 0:
                     print("\n'"+user+"' is a Super user, excluding...")
                 else:
-                    print("\n'"+user+"' is a Normal user, continuing for deletion process...")
+                    print("\n'"+user+"' is a Normal user, continuing for deletion process...\n")
                     user_del()
 
             def user_del():
@@ -66,8 +61,8 @@ def del_operation():
                 check_user_del = subprocess.Popen('p4 users | grep -w ' + user, shell=True, stdout=subprocess.PIPE)
                 user_status = check_user_del.communicate()[0]
                 if user_status:
-                    # print("\n'" + user + "' deletion failed. Checking for shelved files...")
-                    print("\nTrying other methods...")
+                    print("\n'" + user + "' deletion unsuccessful. Checking for shelved files...")
+                    # print("\nTrying other methods...")
                     shelve_file()
                 else:
                     # user deleted successfully
@@ -78,41 +73,49 @@ def del_operation():
                 check_shelve = subprocess.Popen('p4 changes -s pending -u '+user, shell=True, stdout=subprocess.PIPE)
                 shelve = check_shelve.communicate()[0]
                 if shelve:
-                    # print("\n'"+user+"' has shelved files, listing client workspace for deletion...")
+                    print("\n'"+user+"' has shelved files, listing client workspace for deletion...")
                     workspace_del()
-                    check_open_files()
                 else:
-                    # print("\n'"+user+"' has no shelved files, checking for opened files...")
+                    print("\n'"+user+"' has no shelved files, checking for opened files...")
                     check_open_files()
 
             def workspace_del():
-                check_clients = "p4 changes -s pending -u "+user+" |awk {'print $6'}|cut -d '@' -f2 >"+f_mid
+                check_clients = "p4 changes -s pending -u "+user+" |awk {'print $6'}|cut -d '@' -f2 | awk '!x[$0]++'>"+f_mid
                 os.system(check_clients)
                 with open(f_mid, 'r') as fh:
                     for client in fh:
                         del_workspace = "p4 client -df -Fs "+client
                         os.system(del_workspace)
+
                 remove_files()
-                user_del()
+
+                check_deletion = "p4 user -df " + user
+                os.system(check_deletion)
+                check_user_del = subprocess.Popen('p4 users | grep -w ' + user, shell=True, stdout=subprocess.PIPE)
+                user_status = check_user_del.communicate()[0]
+                if user_status:
+                    check_open_files()
+                else:
+                    # user deleted successfully
+                    with open(f_output, "a") as fh:
+                        print(user, file=fh)
 
             def check_open_files():
                 opened_file = subprocess.Popen('p4 -z maxScanRows=2000000 opened -u '+user, shell=True,
                                                stdout=subprocess.PIPE)
                 status_opened = opened_file.communicate()[0]
                 if status_opened:
-                    # print("Deleting opened files...")
-                    open_del = subprocess.Popen("p4 -z maxScanRows=2000000 opened "
-                                                "-u"+user+"|cut -d '@' -f2 |awk {'print $1'}| awk '!x[$0]++' >"+f_last,
-                                                shell=True, stdout=subprocess.PIPE)
+                    print("\nDeleting opened files")
+                    open_del = subprocess.Popen("p4 -z maxScanRows=2000000 opened -u" + user + "|cut -d '@' -f2 |awk {'print $1'}| awk '!x[$0]++' >"+ f_last, shell=True, stdout=subprocess.PIPE)
                     status_open_del = open_del.communicate()[0]
                     with open(f_last, 'r') as fh:
                         for i in fh:
                             del_open = subprocess.Popen("p4 client -df -Fs "+i, shell=True, stdout=subprocess.PIPE)
                             sts_del_open = del_open.communicate()[0]
                     user_del()
-
                 else:
-                    user_del()
+                    print("\nSkipping deletion of user"+user)
+                    pass
 
             p4user()
 
@@ -147,6 +150,10 @@ def remove_files():
         pass
 
 
+if os.path.exists(f_output):
+    os.remove(f_output)
+else:
+    pass
 del_operation()
 total_users()
 remove_files()
